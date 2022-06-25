c9762eab7bdf623d04aef66d7ade7185244e33d3
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-25 20:34:17 +0430

    feat: add config

--- internal/qsse/config.go
+++ internal/qsse/config.go
@@ -2,6 +2,11 @@ package qsse
 
 // Config configuration struct of QSSE.
 type Config struct {
-	Topics []string `koanf:"topics"`
-	Port   int      `koanf:"port"`
+	Topics                    []string `koanf:"topics"`
+	Port                      int      `koanf:"port"`
+	ClientAcceptorCount       int      `koanf:"client_acceptor_count"`
+	ClientAcceptorQueueSize   int      `koanf:"client_acceptor_queue_size"`
+	EventDistributorCount     int      `koanf:"event_distributor_count"`
+	EventDistributorQueueSize int      `koanf:"event_distributor_queue_size"`
+	CleaningInterval          int      `koanf:"cleaning_interval"`
 }
